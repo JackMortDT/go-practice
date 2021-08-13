9ba9367dd4e73a5816bf90b4750176721741acce
Jack <luis.6496@hotmail.com>
2021-08-12 20:59:01 -0500

    Finishing day two with input file

--- pkg/advent2015/dayTwo.go
+++ pkg/advent2015/dayTwo.go
@@ -1,6 +1,7 @@
 package advent2015
 
 import (
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -22,17 +23,11 @@ func CalculatePresentsDimensions(dimensions string) int {
 
 func calculatePresentDimension(dimensions string) int {
 	numbers := strings.Split(dimensions, "x")
-	l, w, h := convertNumbersToInt(numbers)
+	intList := convertToIntList(numbers)
+	l, w, h := intList[0], intList[1], intList[2]
 	smallSide := getSmallSide(l, w, h)
 	result := (2*l*w + 2*w*h + 2*h*l + smallSide)
 	return result
-}
-
-func convertNumbersToInt(numbers []string) (int, int, int) {
-	l, _ := strconv.Atoi(numbers[0])
-	w, _ := strconv.Atoi(numbers[1])
-	h, _ := strconv.Atoi(numbers[2])
-	return l, w, h
 }
 
 func getSmallSide(l int, w int, h int) int {
@@ -45,4 +40,41 @@ func getSmallSide(l int, w int, h int) int {
 		}
 	}
 	return smallest
+}
+
+func CalculateSmallestDimension(dimensions string) int {
+	newDimensions := strings.Split(dimensions, "\n")
+	resultPresent := 0
+	for _, present := range newDimensions {
+		resultPresent += calculateSmallest(present)
+	}
+	return resultPresent		
+}
+
+func calculateSmallest(dimensions string) int {
+	numbers := strings.Split(dimensions, "x")
+	intList := convertToIntList(numbers)
+	l, w, h := intList[0], intList[1], intList[2]
+	feetOfRibbon := calculateFeetOfRibbon(intList)
+	ribbonForTheBow := (l * w * h)
+	result := ribbonForTheBow + feetOfRibbon
+	return result
+}
+
+func calculateFeetOfRibbon(numbers []int) int {
+	sort.Ints(numbers)
+	return sumSmallestSides(numbers[0], numbers[1])
+}
+
+func sumSmallestSides(side1 int, side2 int) int {
+	return (side1*2 + side2*2)
+}
+
+func convertToIntList(list []string) []int {
+	var intList = []int{}
+	for _, number := range list {
+		newNumber, _ := strconv.Atoi(number)
+		intList = append(intList, newNumber)
+	}
+	return intList
 }
--- test/advent2015/Day2/dayTwoPartTwo_test.go
+++ test/advent2015/Day2/dayTwoPartTwo_test.go
@@ -1,1 +1,45 @@
 package advent2015
+
+import (
+	"io/ioutil"
+	"testing"
+	"my.app/pkg/advent2015"
+)
+
+func TestFirstWrapAPresent(t *testing.T) {
+	dimensions := "2x3x4"
+
+	want := 34
+	if got := advent2015.CalculateSmallestDimension(dimensions); got != want {
+		t.Errorf("CalculatePresentsDimensions = %d, want %d", got, want)
+	}
+}
+
+func TestSecondWrapAPresent(t *testing.T) {
+	dimensions := "1x1x10"
+
+	want := 14
+	if got := advent2015.CalculateSmallestDimension(dimensions); got != want {
+		t.Errorf("CalculatePresentsDimensions = %d, want %d", got, want)
+	}
+}
+
+func TestThirdWrapAPresent(t *testing.T) {
+	dimensions := "1x1x10\n1x1x10"
+
+	want := 28
+	if got := advent2015.CalculateSmallestDimension(dimensions); got != want {
+		t.Errorf("CalculatePresentsDimensions = %d, want %d", got, want)
+	}
+}
+
+func TestFinalWrapAPresent(t *testing.T) {
+	input, _ := ioutil.ReadFile("./../../../resources/advent2015/dayTwo_input.txt")
+
+	dimensions := string(input)
+
+	want := 3737498
+	if got := advent2015.CalculateSmallestDimension(dimensions); got != want {
+		t.Errorf("CalculatePresentsDimensions = %d, want %d", got, want)
+	}
+}
